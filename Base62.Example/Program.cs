6b74748729180c66cbfe17f8e941c7a56f11fd56
Kevin <862860000@qq.com>
2019-01-16 20:30:29 +0800

    Initial code
    初始化项目代码

---
+++ Base62.Example/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Base62.Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                string str = Base62.Encode(i);
+                Console.Write($"{str}:{Base62.Decode(str)}\t");
+                if (i % 10 == 0)
+                {
+                    Console.WriteLine();
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Base62/ListExtension.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Base62
+{
+    /// <summary>
+    /// Listk扩展方法
+    /// </summary>
+    public static class ListExtension
+    {
+        public static string ToEncodeString(this List<char> result)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < result.Count; i++)
+            {
+                sb.Append(result[i]);
+            }
+            return sb.ToString();
+        }
+    }
+}
